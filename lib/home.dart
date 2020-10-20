2635d04f07fa1a8533498cc133759eec537e7e19
mpa19 <marcperezarnaiz@gmail.com>
2020-10-20 16:04:01 +0200

    Fixed name files

---
+++ lib/home.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/BoardScreen.dart';
+import 'package:flutter_app/CarouselVerticalScreen.dart';
+import 'package:flutter_app/generated/l10n.dart';
+
+
+import 'CarouselScreen.dart';
+import 'SearchScreen.dart';
+import 'SelectImageScreen.dart';
+import 'SharedPreferences.dart';
+import 'TabMenuScreen.dart';
+import 'generated/l10n.dart';
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  /*final FirebaseMessaging _fcm = FirebaseMessaging();
+
+  @override
+  void initState() {
+    super.initState();
+    _fcm.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            content: ListTile(
+              title: Text(message['notification']['title']),
+              subtitle: Text(message['notification']['body']),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        );
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+  }*/
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MAIN"),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ButtonTheme(
+                buttonColor: Color(0xFF031e39),
+                child: FlatButton(
+                  color: Colors.blueGrey, //Color(0xFF81A483),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => SharedPreferencesScreen()
+                    ),
+                    );
+                  },
+                  child: Text(S.current.preferencesText, style: TextStyle(color: Colors.white),
+                  ),
+                )
+            ),
+            ButtonTheme(
+                buttonColor: Color(0xFF031e39),
+                child: FlatButton(
+                  color: Colors.blueGrey, //Color(0xFF81A483),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => SelectImageScreen()
+                    ),
+                    );
+                  },
+                  child: Text(S.current.selImageText, style: TextStyle(color: Colors.white),
+                  ),
+                )
+            ),
+            ButtonTheme(
+                buttonColor: Color(0xFF031e39),
+                child: FlatButton(
+                  color: Colors.blueGrey, //Color(0xFF81A483),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => CarouselScreen()
+                    ),
+                    );
+                  },
+                  child: Text('Carousel Images',style: TextStyle(color: Colors.white),
+                  ),
+                )
+            ),
+            ButtonTheme(
+                buttonColor: Color(0xFF031e39),
+                child: FlatButton(
+                  color: Colors.blueGrey, //Color(0xFF81A483),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => SearchScreen()
+                    ),
+                    );
+                  },
+                  child: Text('Search Bar',style: TextStyle(color: Colors.white),
+                  ),
+                )
+            ),
+            ButtonTheme(
+                buttonColor: Color(0xFF031e39),
+                child: FlatButton(
+                  color: Colors.blueGrey, //Color(0xFF81A483),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => TabMenuScreen()
+                    ),
+                    );
+                  },
+                  child: Text('Tab Menu with Firebase',style: TextStyle(color: Colors.white),
+                  ),
+                )
+            ),
+            ButtonTheme(
+                buttonColor: Color(0xFF031e39),
+                child: FlatButton(
+                  color: Colors.blueGrey, //Color(0xFF81A483),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => CarouselVerticalScreen()
+                    ),
+                    );
+                  },
+                  child: Text('Carousel Vertical Images',style: TextStyle(color: Colors.white),
+                  ),
+                )
+            ),
+            ButtonTheme(
+              buttonColor: Color(0xFF031e39),
+              child: FlatButton(
+                color: Colors.blueGrey, //Color(0xFF81A483),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                      builder: (context) => BoardScreen()
+                  ),
+                  );
+                },
+                child: Text('Board',style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,171 +1,429 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/BoardScreen.dart';
-import 'package:flutter_app/CarouselVerticalScreen.dart';
-import 'package:flutter_app/generated/l10n.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_app/utilities/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-
-import 'CarouselScreen.dart';
-import 'SearchScreen.dart';
-import 'SelectImageScreen.dart';
-import 'SharedPreferences.dart';
-import 'TabMenuScreen.dart';
 import 'generated/l10n.dart';
+import 'home.dart';
 
-class MyHomePage extends StatefulWidget {
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        S.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      home: LogInScreen(),
+    );
+  }
+}
+
+class LogInScreen extends StatefulWidget {
+
+  @override
+  LogInState createState() => LogInState();
 
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  /*final FirebaseMessaging _fcm = FirebaseMessaging();
+class LogInState extends State<LogInScreen> {
+  bool _rememberMe = false;
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  FirebaseAuth _auth;
+  GoogleSignIn googleSignIn;
 
   @override
   void initState() {
     super.initState();
-    _fcm.configure(
-      onMessage: (Map<String, dynamic> message) async {
-        print("onMessage: $message");
-        showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            content: ListTile(
-              title: Text(message['notification']['title']),
-              subtitle: Text(message['notification']['body']),
+    Firebase.initializeApp().whenComplete(() {
+      setState(() {});
+      _auth = FirebaseAuth.instance;
+      googleSignIn = GoogleSignIn();
+    });
+  }
+
+
+
+  Widget _buildEmailTF() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Email',
+          style: kLabelStyle,
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: kBoxDecorationStyle,
+          height: 60.0,
+          child: TextField(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'OpenSans',
             ),
-            actions: <Widget>[
-              FlatButton(
-                child: Text('Ok'),
-                onPressed: () => Navigator.of(context).pop(),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.white,
               ),
-            ],
+              hintText: 'Enter your Email',
+              hintStyle: kHintTextStyle,
+            ),
           ),
-        );
-      },
-      onLaunch: (Map<String, dynamic> message) async {
-        print("onLaunch: $message");
-      },
-      onResume: (Map<String, dynamic> message) async {
-        print("onResume: $message");
-      },
+        ),
+      ],
     );
-  }*/
+  }
+
+  Widget _buildPasswordTF() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Password',
+          style: kLabelStyle,
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: kBoxDecorationStyle,
+          height: 60.0,
+          child: TextField(
+            controller: _passwordController,
+            obscureText: true,
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'OpenSans',
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.white,
+              ),
+              hintText: 'Enter your Password',
+              hintStyle: kHintTextStyle,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildForgotPasswordBtn() {
+    return Container(
+      alignment: Alignment.centerRight,
+      child: FlatButton(
+        onPressed: () => print('Forgot Password Button Pressed'),
+        padding: EdgeInsets.only(right: 0.0),
+        child: Text(
+          'Forgot Password?',
+          style: kLabelStyle,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRememberMeCheckbox() {
+    return Container(
+      height: 20.0,
+      child: Row(
+        children: <Widget>[
+          Theme(
+            data: ThemeData(unselectedWidgetColor: Colors.white),
+            child: Checkbox(
+              value: _rememberMe,
+              checkColor: Colors.green,
+              activeColor: Colors.white,
+              onChanged: (value) {
+                setState(() {
+                  _rememberMe = value;
+                });
+              },
+            ),
+          ),
+          Text(
+            'Remember me',
+            style: kLabelStyle,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLoginBtn() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: () {
+          _signInWithEmailAndPassword();
+        },
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        color: Colors.white,
+        child: Text(
+          'LOGIN',
+          style: TextStyle(
+            color: Color(0xFF527DAA),
+            letterSpacing: 1.5,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'OpenSans',
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSignInWithText() {
+    return Column(
+      children: <Widget>[
+        Text(
+          '- OR -',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        SizedBox(height: 20.0),
+        Text(
+          'Sign in with',
+          style: kLabelStyle,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSocialBtn(Function onTap, AssetImage logo) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 60.0,
+        width: 60.0,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0, 2),
+              blurRadius: 6.0,
+            ),
+          ],
+          image: DecorationImage(
+            image: logo,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSocialBtnRow() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 30.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildSocialBtn(
+                () {
+              Navigator.push(context, MaterialPageRoute(
+                  builder: (context) => MyHomePage()
+              ),
+              );
+            },
+            AssetImage(
+              'assets/images/facebook.png',
+            ),
+          ),
+          _buildSocialBtn(
+                () {
+              signInWithGoogle().then((result) {
+                if (result != null) {
+                  Navigator.push(context, MaterialPageRoute(
+                      builder: (context) => MyHomePage()
+                  ),
+                  );
+                }
+              });
+            },
+            AssetImage(
+              'assets/images/google.png',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSignupBtn() {
+    return GestureDetector(
+      onTap: () => print('Sign Up Button Pressed'),
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: 'Don\'t have an Account? ',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            TextSpan(
+              text: 'Sign Up',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _signInWithEmailAndPassword() async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: _emailController.text,
+          password: _passwordController.text
+      );
+      Navigator.push(context, MaterialPageRoute(
+          builder: (context) => MyHomePage()
+      ),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
+
+  Future<String> signInWithGoogle() async {
+    await Firebase.initializeApp();
+
+    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
+
+    final UserCredential authResult = await _auth.signInWithCredential(credential);
+    final User user = authResult.user;
+
+    if (user != null) {
+      assert(!user.isAnonymous);
+      assert(await user.getIdToken() != null);
+
+      final User currentUser = _auth.currentUser;
+      assert(user.uid == currentUser.uid);
+
+      print('signInWithGoogle succeeded: $user');
+
+      return '$user';
+    }
+
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("MAIN"),
-        ),
-
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Stack(
             children: <Widget>[
-              ButtonTheme(
-                  buttonColor: Color(0xFF031e39),
-                  child: FlatButton(
-                    color: Colors.blueGrey, //Color(0xFF81A483),
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(
-                          builder: (context) => SharedPreferencesScreen()
-                      ),
-                      );
-                    },
-                    child: Text(S.current.preferencesText, style: TextStyle(color: Colors.white),
-                    ),
-                  )
-              ),
-              ButtonTheme(
-                  buttonColor: Color(0xFF031e39),
-                  child: FlatButton(
-                    color: Colors.blueGrey, //Color(0xFF81A483),
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(
-                          builder: (context) => SelectImageScreen()
-                      ),
-                      );
-                    },
-                    child: Text(S.current.selImageText, style: TextStyle(color: Colors.white),
-                    ),
-                  )
-              ),
-              ButtonTheme(
-                  buttonColor: Color(0xFF031e39),
-                  child: FlatButton(
-                    color: Colors.blueGrey, //Color(0xFF81A483),
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(
-                          builder: (context) => CarouselScreen()
-                      ),
-                      );
-                    },
-                    child: Text('Carousel Images',style: TextStyle(color: Colors.white),
-                    ),
-                  )
-              ),
-              ButtonTheme(
-                  buttonColor: Color(0xFF031e39),
-                  child: FlatButton(
-                    color: Colors.blueGrey, //Color(0xFF81A483),
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(
-                          builder: (context) => SearchScreen()
-                      ),
-                      );
-                    },
-                    child: Text('Search Bar',style: TextStyle(color: Colors.white),
-                    ),
-                  )
-              ),
-              ButtonTheme(
-                  buttonColor: Color(0xFF031e39),
-                  child: FlatButton(
-                    color: Colors.blueGrey, //Color(0xFF81A483),
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(
-                          builder: (context) => TabMenuScreen()
-                      ),
-                      );
-                    },
-                    child: Text('Tab Menu with Firebase',style: TextStyle(color: Colors.white),
-                    ),
-                  )
-              ),
-              ButtonTheme(
-                  buttonColor: Color(0xFF031e39),
-                  child: FlatButton(
-                    color: Colors.blueGrey, //Color(0xFF81A483),
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(
-                          builder: (context) => CarouselVerticalScreen()
-                      ),
-                      );
-                    },
-                    child: Text('Carousel Vertical Images',style: TextStyle(color: Colors.white),
-                    ),
-                  )
-              ),
-              ButtonTheme(
-                buttonColor: Color(0xFF031e39),
-                child: FlatButton(
-                  color: Colors.blueGrey, //Color(0xFF81A483),
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(
-                        builder: (context) => BoardScreen()
-                    ),
-                    );
-                  },
-                  child: Text('Board',style: TextStyle(color: Colors.white),
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xFF73AEF5),
+                      Color(0xFF61A4F1),
+                      Color(0xFF478DE0),
+                      Color(0xFF398AE5),
+                    ],
+                    stops: [0.1, 0.4, 0.7, 0.9],
                   ),
                 ),
               ),
+              Container(
+                height: double.infinity,
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40.0,
+                    vertical: 120.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Sign In',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'OpenSans',
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 30.0),
+                      _buildEmailTF(),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      _buildPasswordTF(),
+                      _buildForgotPasswordBtn(),
+                      _buildRememberMeCheckbox(),
+                      _buildLoginBtn(),
+                      _buildSignInWithText(),
+                      _buildSocialBtnRow(),
+                      _buildSignupBtn(),
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
-      );
+      ),
+    );
   }
-
-
 }
