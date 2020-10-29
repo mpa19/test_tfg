aea77f9859d84a0ed16849cb84ef12aa9d69aa2d
mpa19 <marcperezarnaiz@gmail.com>
2020-10-29 17:33:50 +0100

    Upload image to server

--- lib/SelectImageScreen.dart
+++ lib/SelectImageScreen.dart
@@ -1,9 +1,14 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:image_picker/image_picker.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+import 'package:path/path.dart';
 
 class SelectImageScreen extends StatefulWidget {
 
@@ -14,10 +19,31 @@ class SelectImageScreen extends StatefulWidget {
 class _SelectImageState extends State<SelectImageScreen> {
   File _image;
   final picker = ImagePicker();
+  String msg = '';
+  Future<List> _uploadImage() async {
+    final response = await http.post(
+        "https://www.martabatalla.com/flutter/wenect/profileImages/uploadImage.php",
+        body: {
+          "image": base64Encode(_image.readAsBytesSync()),
+          "name": "1"+basename(_image.path)
+        });
+
+    var datauser = json.decode(response.body);
+
+    if(datauser.length==0){
+      setState(() {
+        msg="Upload Fail";
+      });
+    }else{
+      msg="Upload Successfully";
+    }
+    return datauser;
+  }
 
   Future getImage() async {
     final pickedFile = await picker.getImage(source: ImageSource.gallery); // or ImageSource.camera to do a photo
 
+    // /public_html/flutter/wenect/profileImages
     setState(() {
       _image = File(pickedFile.path);
     });
@@ -30,24 +56,40 @@ class _SelectImageState extends State<SelectImageScreen> {
           title: new Text("Select an image"),
         ),
       body: Center(
-        child: CircleAvatar(
-          radius: 100,
-          backgroundColor: Colors.red,
-          child: CircleAvatar(
-              radius: 70,
-              backgroundColor: Color(0xff476cfb),
-            child: ClipOval(
-              child: SizedBox(
-                  width: 180,
-                  height: 180,
-                  child: _image == null
-                      ? Image.asset('assets/images/defaultuser.png',
-                      fit: BoxFit.fill
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                  radius: 100,
+                  backgroundColor: Colors.red,
+                  child: CircleAvatar(
+                    radius: 70,
+                    backgroundColor: Color(0xff476cfb),
+                    child: ClipOval(
+                      child: SizedBox(
+                          width: 180,
+                          height: 180,
+                          child: _image == null
+                              ? Image.asset('assets/images/defaultuser.png',
+                              fit: BoxFit.fill
+                          )
+                              : Image.file(_image)
+                      ),
+                    ),
                   )
-                      : Image.file(_image)
               ),
-            ),
-          )
+              ButtonTheme(
+                  buttonColor: Color(0xFF031e39),
+                  child: FlatButton(
+                    color: Colors.blueGrey, //Color(0xFF81A483),
+                    onPressed: () {
+                      _uploadImage();
+                    },
+                    child: Text("Upload Image", style: TextStyle(color: Colors.white),
+                    ),
+                  )
+              ),
+              ]
         )
       ),
       floatingActionButton: FloatingActionButton(
